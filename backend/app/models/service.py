3df16895d3d1300f9c8ba7fb7344573e3b3636d9
bryan-ahnlab <bryan@ahnlab.io>
2025-06-10 08:08:09 +0000

    fix: change long_url column type to String(255) for consistency

--- backend/app/models/service.py
+++ backend/app/models/service.py
@@ -7,5 +7,5 @@ class URL(Base):
     __tablename__ = "URLS"
     
     id = Column(CHAR(32), primary_key=True, default=lambda: uuid.uuid4().hex, unique=True, nullable=False)
-    long_url = Column(LONGTEXT, index=True)
+    long_url = Column(String(255), index=True)
     short_url = Column(String(255), unique=True, index=True)
